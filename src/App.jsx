37c421ff631167fe7a7c7b4c469d47c6a5b68af4
Bilal <bilalmuhammadyousuf543@gmail.com>
2023-09-14 20:21:23 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import TextForm from './textForm'
+
+function App() {
+
+  return (
+    <>
+      <div className="container">
+        <TextForm heading='Word Counter App' />
+      </div>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/textForm.jsx
@@ -0,0 +1,57 @@
+// import ProtoTypes from 'prop-types'
+import { useState } from 'react'
+
+
+export default function TextForm(props) {
+    const handleUpperCase = () => {
+        setText('You have cicked to upper-case')
+        let newText = text.toUpperCase()
+        setText(newText)
+    }
+
+    const handleLowerCase = () => {
+        setText('You have cicked to upper-case')
+        let newText = text.toLowerCase()
+        setText(newText)
+    }
+
+    const handleCopyText = () => {
+        let newText = document.getElementById('textarea')
+        newText.Select
+        navigator.clipboard.writeText(newText.value)
+        alert('Text copied!')
+    }
+
+    const handleRemoveExtraSpace = () => {
+        let newText = text.split(`/[]+ /s`)
+        setText(newText.join(''))
+    }
+
+    const handleOnChange = (event) => {
+        setText(event.target.value)
+    }
+    const [text, setText] = useState('')
+    return (
+        <>
+            <h1>{props.heading}</h1>
+            <div className="box">
+                <textarea className='text-area' value={text} onChange={handleOnChange} placeholder='Enter text here' id='textarea'>
+                </textarea>
+
+                <div className="button">
+                    <button onClick={handleUpperCase}>Uppercase</button>
+                    <button onClick={handleLowerCase}>Lowercase</button>
+                    <button onClick={handleCopyText}>Copy Text</button>
+                    <button onClick={handleRemoveExtraSpace}>Remove Extra Space</button>
+                </div>
+            </div>
+
+            <div className="para">
+                <p><strong>Total No of Words:</strong> {text.split(' ').length - 1}</p>
+                <p><strong>Total No of Characters:</strong> {text.length}</p>
+                <p><strong>Time-Taken:</strong> {0.008 * text.split("").length}</p>
+                <p><strong>Text Preview:</strong> {text}</p>
+            </div>
+        </>
+    )
+}
